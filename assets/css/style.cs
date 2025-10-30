1ee173a172350a5985dbbc57360dd08417377fb5
Eileen Chen <ec.eileenchen@gmail.com>
2025-10-30 14:17:11 -0700

    Create style.cs

---
+++ assets/css/style.cs
@@ -0,0 +1,11 @@
+.btn-blue {
+  background-color: #007acc;
+  color: white;
+  border-radius: 6px;
+  padding: 0.4em 0.8em;
+  text-decoration: none;
+}
+
+.btn-blue:hover {
+  background-color: #005f99;
+}
